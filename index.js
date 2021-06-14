ec01f9f825d43f7b74431f756c3427d6dfe6b227
Nicolas Olivares <nolivaresr@hotmail.com>
2021-06-14 01:45:58 -0400

    beginning

---
+++ index.js
@@ -0,0 +1,10 @@
+const disclaimer = document.getElementById('disclaimer');
+const light = document.getElementById('light');
+const button = document.getElementById('light-button');
+
+
+
+button.on('click', () => {
+    // disclaimer.attributes('class', 'invisible');
+    console.log('button pushed');
+})
